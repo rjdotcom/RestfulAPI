055b322c07bc0c6a64fa1108cddf1ecaacc7396c
rjdotcom <jeffersonrgabriel@icloud.com>
2023-01-19 02:12:11 -0500

    update database

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 
 //Database connection
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+mongoose.connect("mongodb://127.0.0.1:27017/articlesDB");
 
 const articleSchema = new mongoose.Schema({
   title: String,
